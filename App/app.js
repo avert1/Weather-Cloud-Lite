d8f9e03589f166b59817a1bd5e70ebb4cc8f2599
Avery H <hortonavery@gmail.com>
2020-08-16 20:49:40 -0700

    Adding rpi-ws281x-native to requirements for interacting with ws2812b

--- App/app.js
+++ App/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const currentWeatherService = require("./Services/weather-service");
 
 const App = express();
+
+let currentWeather;
 
 App.use(express.static(__dirname + '/public'));
 
@@ -8,7 +11,11 @@ App.get('/', (req, res) => {
   res.sendFile(__dirname + '/Views/index.html');
 });
 
-//Get weather data every 5 minutes
-
-
+/*
+//Get weather data every 5 minutes. Move to other service.
+currentWeatherService.getCurrentWeather().then(res => res.json())
+.then(resJson => {
+  currentWeather =
+})
+*/
 module.exports = App;
