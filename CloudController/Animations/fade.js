1da7c514b79a761fa3bbe78537c2d5a166e6cfe8
Avery H <hortonavery@gmail.com>
2020-08-22 03:50:23 -0700

    fixing fade method bug, lowering max of fade brightness

--- CloudController/Animations/fade.js
+++ CloudController/Animations/fade.js
@@ -12,27 +12,28 @@ class fade extends LEDStripAnim {
 
   run() {
     for (let i = 0; i < this.numLeds; i++) {
-      this.pixelData[i] = helpers.rgb2Int(0,0,0);
+      this.pixelData[i] = helpers.rgb2Int(255,255,255);
     }
+    ws281x.setBrightness(0);
     ws281x.render(this.pixelData);
 
-    this.interval = setInterval(function () {
+    this.interval = setInterval(() => {
       if(this.increasing) {
         this.brightnessOffset += 1;
-        if(this.brightnessOffset >= 255) {
+        if(this.brightnessOffset >= 220) {
           this.increasing = false;
-          this.brightnessOffset = 255;
+          this.brightnessOffset = 220;
         }
       }
       else {
         this.brightnessOffset -=1;
-        if(this.brightnessOffset <= 0) {
+        if(this.brightnessOffset <= 1) {
           this.increasing = true;
-          this.brightnessOffset = 0;
+          this.brightnessOffset = 1;
         }
       }
       ws281x.setBrightness(this.brightnessOffset);
-     }, 1000 / 30);
+    }, 1000 / 80);
    }
 }
 
